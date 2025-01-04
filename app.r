af69bc056a816aad4e7cded28165b8f509b7b375
tippijayanthreddy <tippijayanthreddy@gmail.com>
2025-01-03 23:21:51 -0500

    Add README and app.R files

---
+++ app.r
@@ -0,0 +1,21 @@
+library(shiny)
+
+ui <- fluidPage(
+    titlePanel("Simple Shiny App"),
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("slider", "Select a value:", min = 1, max = 100, value = 50)
+        ),
+        mainPanel(
+            textOutput("output")
+        )
+    )
+)
+
+server <- function(input, output) {
+    output$output <- renderText({
+        paste("You selected:", input$slider)
+    })
+}
+
+shinyApp(ui = ui, server = server)
