81ec03349b11de79529a7d5bfdb6cfa3ddc0d6d7
tippijayanthreddy <54163140+tippijayanthreddy@users.noreply.github.com>
2025-01-05 15:26:03 -0500

    Update app.r

--- app.r
+++ app.r
@@ -18,4 +18,8 @@ server <- function(input, output) {
     })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(
+    ui = ui,
+    server = server,
+    options = list(host = "0.0.0.0", port = 3838)
+)
