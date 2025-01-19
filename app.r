eedc4b3462510d0fdf81cf50aaf931db1b49ca60
tippijayanthreddy <tippijayanthreddy@gmail.com>
2025-01-18 20:53:46 -0500

    updated

--- app.r
+++ app.r
@@ -1,5 +1,3 @@
-library(shiny)
-
 ui <- fluidPage(
     titlePanel("Simple Shiny App"),
     sidebarLayout(
@@ -23,9 +21,4 @@ shinyApp(
     server = server,
     options = list(host = "0.0.0.0", port = 3562)
 )
-- name: Copy R Script to EC2
-      run: |
-        scp -o StrictHostKeyChecking=no app.r ec2-user@54.164.80.110:~/simple-r-app
-    - name: Execute R Script on EC2
-      run: |
-        ssh -o StrictHostKeyChecking=no ec2-user@54.164.80.110 "Rscript ~/simple-r-app/app.r"
+ 
